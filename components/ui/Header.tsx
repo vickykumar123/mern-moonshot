87ced28fcf2b8ae16e47f30a85a12e4fb157bbb6
vicky <vickykumarsunilja@gmail.com>
2024-03-21 17:58:21 +0530

    Added header section in UI

---
+++ components/ui/Header.tsx
@@ -0,0 +1,44 @@
+import {Search, ShoppingCart} from "lucide-react";
+
+export default function Header() {
+  return (
+    <header>
+      <nav className="flex justify-between items-center h-[100px] box-border p-12">
+        <div className="flex flex-col w-full">
+          <div className="flex justify-end space-x-3">
+            <span>Help</span>
+            <span>Orders & Return</span>
+            <span>Hi, John</span>
+          </div>
+          <div className="flex items-center justify-between">
+            <div className="text-[32px] font-[700]">Ecommerce</div>
+            <div className="space-x-12 flex items-center justify-between text-[16px] font-[600]">
+              <span>Categories</span>
+              <span>Sale</span>
+              <span>Clearance</span>
+              <span>New stock</span>
+              <span>Trending</span>
+            </div>
+            <div className="flex justify-between space-x-4">
+              <span>
+                <Search className="w-[32px] text-[32px]" />
+              </span>
+              <span>
+                <ShoppingCart className="w-[32px] text-[32px] " />
+              </span>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <div className="bg-[#F4F4F4] flex items-center justify-center h-[36px]">
+        <section className="flex items-center space-x-5 ">
+          <p className="w-[16px]">&lt; </p>
+          <p className="text-[14px] font-[500]">
+            Get 10% off on business sign up{" "}
+          </p>
+          <p className=" w-[16px]">&gt;</p>
+        </section>
+      </div>
+    </header>
+  );
+}
